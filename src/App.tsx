8466597346d8c31659d6b324543d8876ff022334
bishwashpantha <iambishwashpantha@gmail.com>
2023-01-09 18:26:46 +0545

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
+import Home from './pages/Home'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path='/watch/:id' element={<Watch/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+    <div className='max-h-screen overflow-hidden'>
+      <div style={{height: "7.5vh"}}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{height: '92.5vh'}}>
+        
+      </div>
+    </div>
+  )
+}
